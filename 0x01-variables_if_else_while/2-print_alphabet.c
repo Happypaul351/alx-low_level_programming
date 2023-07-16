5329767d0e1ae103531fb81b5706399b4797f6e9
Happy Happy <happypaul351@gmail.com>
2023-07-16 08:18:16 -0700

    i sometimes suffers from insonmia and when i cant fall asleep i play what i called the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entrance point
+ * Return: 0.
+ */
+
+int main()
+{
+	char alpbet = 'a';
+
+	for (alpbet = 'a'; alpbet <= 'z'; alpbet++)
+		putchar(alpbet);
+	putchar('\n');
+
+	return (0);
+}
