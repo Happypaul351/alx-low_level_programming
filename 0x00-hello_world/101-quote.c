1761771f1ff1547d4b36cbc11cf0d220c9394bef
Happy Happy <happypaul351@gmail.com>
2023-07-17 17:29:27 -0700

    art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,7 @@
  */
 int main(void)
 {
-	int ch = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
-
-	putchar(ch);
+	write('and that piece of art is useful" - Dora Korpar, 2015-10-19');
 
 	return (1);
 }
