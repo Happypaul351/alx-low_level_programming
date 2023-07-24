fe6dc4e8f1b74442c41510e5f3baff8c9b74a2f4
Happy Happy <happypaul351@gmail.com>
2023-07-24 06:40:55 -0700

    i do not fear computers, i fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - functions that prints a string to the stdout
+ * @str: the string
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	char *str;
+}
