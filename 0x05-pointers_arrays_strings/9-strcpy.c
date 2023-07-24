c445cd262afe26d1256cb3b945acf3db58ca437a
Happy Happy <happypaul351@gmail.com>
2023-07-24 07:17:52 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - copy the string pointed to
+ * @dest: destination pointer
+ * @src: the source pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *dest;
+	char *src;
+}
