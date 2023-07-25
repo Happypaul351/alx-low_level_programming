f36bb0184e880f19961ef94de7bd872625e54295
Happy Happy <happypaul351@gmail.com>
2023-07-25 04:40:53 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,5 +7,18 @@
 
 int main(void)
 {
+	int comb = 0;
+
+	while (comb <= 9)
+	{
+		putchar(comb + '0');
+		if (comb < 9)
+		{
+			putchar (',');
+			putchar(' ');
+		}
+		comb++;
+	}
+		putchar('\n');
 	return (0);
 }
