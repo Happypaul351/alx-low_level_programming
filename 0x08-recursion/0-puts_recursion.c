389cd2a9dcfaf074bd33bb52b1530c64c5484fd1
Happy Happy <happypaul351@gmail.com>
2023-08-02 10:56:34 -0700

    You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string follow by a new line
+ * @s: the string
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_puts_recursion(s - 1);
+		puts(s);
+	}
+}
