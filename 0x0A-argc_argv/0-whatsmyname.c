ba2306e87a8df72035ca46682a25cc817667a798
Happy Happy <happypaul351@gmail.com>
2023-08-04 15:29:49 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdargs.h>
+
+/**
+ * main - the entry point of the program
+ * @argc: the number of argument
+ * @argv: the vector argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+}
