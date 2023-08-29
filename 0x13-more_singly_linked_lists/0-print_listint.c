714f268c5dd460e2f45246f478054f8580077926
Happy Happy <happypaul351@gmail.com>
2023-08-28 21:23:22 -0700

    header file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_lisstint - prinnts all the elements of a listint_t
+ * @h: head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+
