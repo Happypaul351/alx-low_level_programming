0678e4edc695782fdc5c8b861c2f80080646f8d7
Happy Happy <happypaul351@gmail.com>
2023-08-29 16:46:42 -0700

    Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list 
+ * @head: head of list
+ * Return: none
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *a;
+
+	while (head != NULL)
+	{
+		a = head->next;
+		free(head);
+		head = a;
+	}
+}
