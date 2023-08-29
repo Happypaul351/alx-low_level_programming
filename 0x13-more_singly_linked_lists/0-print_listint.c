937caf821aafc56522a65bf7fd72ed1aa9310a9c
Happy Happy <happypaul351@gmail.com>
2023-08-28 21:31:27 -0700

    task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
 /**
- * print_lisstint - prinnts all the elements of a listint_t
+ * print_listint - prinnts all the elements of a listint_t
  * @h: head of the list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
+	const listint_t *a;
+	size_t counter;
 
+	for (a = h, counter = 0; a != NULL; counter++, a = a->next)
+		printf("%d\n", a->next);
+
+	return (counter);
+}
