02ea302603ea8a814c5b56b4fc154ced96b9c73a
Happy Happy <happypaul351@gmail.com>
2023-08-29 17:00:31 -0700

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function to remove the head node
+ * @head: pointer to the pointer
+ * Return: the value
+ */
+
+int pop_listint(listint_t **head)
+{
+	int a;
+
+	listint_t *del_node;
+	listint_t *next_node;
+
+	if(*head == NULL)
+		return (0);
+
+	del_node = *head;
+	next_node = (*head)->next;
+
+	a = del_node->n;
+	*head = next_node;
+	free(del_node);
+
+	return (a);
+}
