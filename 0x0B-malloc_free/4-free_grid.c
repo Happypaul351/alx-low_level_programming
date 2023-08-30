f8f0295c72983d3eeece703d5fe2fc0159735bd2
Happy Happy <happypaul351@gmail.com>
2023-08-29 21:31:44 -0700

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - entry point
+ * @grid: array
+ * @height: ros
+ */
+
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a <height; a++)
+	{
+
+		free(grid[a]);
+	}
+	free(grid);
+}
